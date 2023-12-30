6f2ecd62be3e7e1bf7538b41aefa9ab9bd1d066e
djon198360 <le1907@mail.ru>
2023-12-30 17:52:56 +0300

    feat Main and Other

---
+++ client/src/Pages/Main/Style.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Main = styled.main``;
+
+export const MainContainer = styled.div`
+  max-width: 1178px;
+  margin: 0 auto;
+  padding: 52px 10px 37px;
+`;
+
+export const MainContainerMob = styled.div`
+  padding: 85px 10px 84px;
+`;
+
+export const MainH2 = styled.h2`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 40px;
+  line-height: 42px;
+  color: #000000;
+  margin-bottom: 30px;
+`;
+
+export MainContent =  styled.div`
+  width: 100%;
+  margin: 0 auto;
+  `;
+
+export const MainContentMob = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  overflow: hidden;
+  position: fixed;
+  right: 0;
+  left: 0;
+  top: 134px;
+  bottom: 84px;
+  `;
